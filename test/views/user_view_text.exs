9553123f545391b250dc00381842bf6871f89492
Alex Koppel <alex@alexkoppel.com>
2017-06-01 19:01:51 -0500

    Fix UserView test

--- test/views/user_view_text.exs
+++ test/views/user_view_text.exs
@@ -9,13 +9,12 @@ defmodule Elmelixirstarter.UserViewTest do
   test "#show renders the user" do
     user = insert(:user)
 
-    assert UserView.render("index.json", %{user: user}) == [
-      %{
-        id: user.id,
-        name: user.name,
-        image_url: user.image_url,
-        username: image.username
-      }
+    assert UserView.render("show.json", %{user: user}) == %{
+      id: user.id,
+      name: user.name,
+      image_url: user.image_url,
+      username: user.username
+    }
   end
 end
 
