6ceb42a426ba58c1440dd2c4f0b546915c632b9a
Alex Koppel <alex+git@alexkoppel.com>
2018-05-10 22:30:02 -0500

    Upgrade to latest Elixir/Node/npm packages/etc. (#28)
    * Update Elixir version and dependencies
    
    * Get Dockerfile compiling again
    
    * Update NPM packages
    
    * Update readme

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Elmelixirstarter.Mixfile do
   def project do
     [app: :elmelixirstarter,
      version: "0.1.0",
-     elixir: "~> 1.4",
+     elixir: "~> 1.6",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix, :gettext] ++ Mix.compilers,
      build_embedded: Mix.env == :prod,
